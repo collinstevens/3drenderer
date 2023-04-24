ede8a6e90d079f7585370c115fa9e38113728b48
Collin Stevens <github@collinstevens.com>
2023-04-24 01:29:46 -0600

    render yellow rectangle from texture

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL.h>
 
@@ -17,8 +18,15 @@ typedef uint32_t u32;
 typedef uint64_t u64;
 
 global_variable bool is_running = false;
+
 global_variable SDL_Window* window = NULL;
 global_variable SDL_Renderer* renderer = NULL;
+
+global_variable u32* color_buffer = NULL;
+global_variable SDL_Texture* color_buffer_texture = NULL;
+
+global_variable u32 window_width = 800;
+global_variable u32 window_height = 600;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -30,8 +38,8 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
 
@@ -51,7 +59,14 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
+    color_buffer = (u32*) malloc(sizeof(u32) * window_width * window_height);
 
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height);
 }
 
 void process_input(void) {
@@ -75,11 +90,46 @@ void update(void) {
 
 }
 
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        window_width * sizeof(u32)
+    );
+
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL
+    );
+}
+
+void clear_color_buffer(u32 color) {
+    for (size_t y = 0; y < window_height; y++) {
+        for (size_t x = 0; x < window_width; x++) {
+            color_buffer[window_width * y + x] = color;
+        }
+    }
+    
+}
+
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
+
     SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    free(color_buffer);
 }
 
 int main(void) {
@@ -92,6 +142,8 @@ int main(void) {
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
